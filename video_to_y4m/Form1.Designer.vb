cc9f220f32f40346b30f44884708e85ca6e5240e
moisesmcardona <moises@moises-studios.com>
2018-10-15 18:44:24 -0400

    Add project files.

---
+++ video_to_y4m/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SaveFileDialog2 = New System.Windows.Forms.SaveFileDialog()
+        Me.GroupBox1.SuspendLayout
+        Me.SuspendLayout
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(279, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Please follow the following simple steps to encode to y4m:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(12, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(152, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Step 1: Browse for input video:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 47)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(261, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(282, 45)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(282, 86)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = true
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(15, 88)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(261, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.Location = New System.Drawing.Point(12, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(170, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Step 2: Browse for output location:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = true
+        Me.Label5.Location = New System.Drawing.Point(12, 144)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(162, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Step 3: Encode or Add to Queue"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(13, 160)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(147, 40)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Encode"
+        Me.Button3.UseVisualStyleBackColor = true
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = true
+        Me.Label7.Location = New System.Drawing.Point(13, 213)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(121, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "v1.0 by Moises Cardona"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(395, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(292, 135)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = false
+        Me.GroupBox1.Text = "Queue"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+            Or System.Windows.Forms.AnchorStyles.Left)  _
+            Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = true
+        Me.ListBox1.Location = New System.Drawing.Point(6, 19)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(279, 95)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = false
+        Me.Button4.Location = New System.Drawing.Point(525, 150)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(79, 36)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "Delete"
+        Me.Button4.UseVisualStyleBackColor = true
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(166, 160)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(147, 40)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "Add to Queue"
+        Me.Button5.UseVisualStyleBackColor = true
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(395, 150)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(124, 36)
+        Me.Button6.TabIndex = 16
+        Me.Button6.Text = "Encode items"
+        Me.Button6.UseVisualStyleBackColor = true
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(610, 150)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(77, 36)
+        Me.Button7.TabIndex = 17
+        Me.Button7.Text = "Clear"
+        Me.Button7.UseVisualStyleBackColor = true
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = true
+        Me.CheckBox1.Location = New System.Drawing.Point(15, 114)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(130, 17)
+        Me.CheckBox1.TabIndex = 21
+        Me.CheckBox1.Text = "Extract audio as WAV"
+        Me.CheckBox1.UseVisualStyleBackColor = true
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 235)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = false
+        Me.Name = "Form1"
+        Me.Text = "Video to y4m"
+        Me.GroupBox1.ResumeLayout(false)
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents SaveFileDialog2 As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ video_to_y4m/Form1.vb
@@ -0,0 +1,105 @@
+﻿Public Class Form1
+    Dim InputFileList As New List(Of String)
+    Dim OutputFileList As New List(Of String)
+    Dim ExtractAudio As New List(Of Boolean)
+    Dim CurrInputFile As String
+    Dim CurrOutputFile As String
+    Dim CurrRotation As Integer
+    Public Sub ProcessStart(ByRef Executable, ByRef CommandLine)
+        Dim P As New Process
+        With P
+            .StartInfo.FileName = Executable
+            .StartInfo.Arguments = CommandLine
+            .StartInfo.RedirectStandardOutput = False
+            .StartInfo.RedirectStandardError = False
+            .StartInfo.UseShellExecute = True
+            .StartInfo.CreateNoWindow = False
+            .Start()
+            .WaitForExit()
+        End With
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text = "" Then
+            MsgBox("An Input video is needed")
+        ElseIf TextBox2.Text = "" Then
+            MsgBox("An Output location is needed")
+        Else
+            CurrInputFile = TextBox1.Text
+            CurrOutputFile = TextBox2.Text
+            ProcessStart("ffmpeg.exe", "-i """ & TextBox1.Text & """ """ & TextBox2.Text & """")
+            If CheckBox1.Checked Then
+                ProcessStart("ffmpeg.exe", "-i """ & TextBox1.Text & """ -vn """ & My.Computer.FileSystem.GetParentPath(TextBox2.Text) & "\" & IO.Path.GetFileNameWithoutExtension(TextBox2.Text) & ".wav""")
+            End If
+            MsgBox("Video processed!")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" = False Then OpenFileDialog1.FileName = My.Computer.FileSystem.GetName(TextBox1.Text) Else OpenFileDialog1.FileName = ""
+        OpenFileDialog1.Filter = "All file types|*.*"
+        OpenFileDialog1.Title = "Select a source video"
+        Dim DialogOk As DialogResult = OpenFileDialog1.ShowDialog
+        If DialogOk = Windows.Forms.DialogResult.OK Then If OpenFileDialog1.FileName = "" = False Then TextBox1.Text = OpenFileDialog1.FileName
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox2.Text = "" = False Then SaveFileDialog2.FileName = My.Computer.FileSystem.GetName(TextBox2.Text) Else SaveFileDialog2.FileName = ""
+        SaveFileDialog2.Filter = "y4m|*.y4m"
+        SaveFileDialog2.Title = "Browse for output"
+        Dim DialogOk As DialogResult = SaveFileDialog2.ShowDialog
+        If DialogOk = Windows.Forms.DialogResult.OK Then If SaveFileDialog2.FileName = "" = False Then TextBox2.Text = SaveFileDialog2.FileName
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If TextBox1.Text = "" Then
+            MsgBox("An Input video is needed")
+        ElseIf TextBox2.Text = "" Then
+            MsgBox("An Output location is needed")
+        Else
+            ListBox1.Items.Add(My.Computer.FileSystem.GetName(TextBox1.Text))
+            InputFileList.Add(TextBox1.Text)
+            OutputFileList.Add(TextBox2.Text)
+            ExtractAudio.Add(CheckBox1.Checked)
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim Index As Integer = ListBox1.SelectedIndex
+        ListBox1.Items.RemoveAt(Index)
+        InputFileList.RemoveAt(Index)
+        OutputFileList.RemoveAt(Index)
+        ExtractAudio.RemoveAt(Index)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedItem = "" = False Then Button4.Enabled = True Else Button4.Enabled = False
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim processeditems As Integer = 0
+        For i As Integer = 0 To ListBox1.Items.Count - 1
+            CurrInputFile = InputFileList(i)
+            CurrOutputFile = OutputFileList(i)
+            CurrRotation = ExtractAudio(i)
+            ProcessStart("ffmpeg.exe", "-i """ & CurrInputFile & """ """ & CurrOutputFile & """")
+            If ExtractAudio(i) Then
+                ProcessStart("ffmpeg.exe", "-i """ & CurrInputFile & """ -vn """ & My.Computer.FileSystem.GetParentPath(CurrOutputFile) & "\" & IO.Path.GetFileNameWithoutExtension(CurrOutputFile) & ".wav""")
+            End If
+            processeditems = processeditems + 1
+        Next
+        MsgBox(processeditems & " Videos processed!")
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        ListBox1.Items.Clear()
+        InputFileList.Clear()
+        OutputFileList.Clear()
+        ExtractAudio.Clear()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not My.Computer.FileSystem.FileExists("ffmpeg.exe") Then
+            MsgBox("ffmpeg.exe not found. You'll be redirected to the ffmpeg download website")
+        End If
+    End Sub
+End Class
